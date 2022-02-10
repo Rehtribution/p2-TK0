c4c2c65aa8ece4c0a6dafe348cd2aba95127bff3
Reht Cavazos <reht.dev@gmail.com>
2022-02-10 16:14:50 -0600

    connected front end to homepage on localhost

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const path = require("path");
-const routes = require('./controllers');
+const routes = require('./controllers/');
 const session = require("express-session");
 const express = require("express");
 const exphbs = require("express-handlebars");
