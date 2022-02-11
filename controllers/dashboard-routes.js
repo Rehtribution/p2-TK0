0b90695b9b3ea867ead4ec359fbf1a55fc44e7de
Reht Cavazos <reht.dev@gmail.com>
2022-02-11 14:10:09 -0600

    spelling, typos, synching

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -7,7 +7,7 @@ const withAuth = require("../utils/auth");
 router.get("/", withAuth, (req, res) => {
 	console.log(req.session);
 	console.log("======================");
-	Post.findAll({
+	Highscore.findAll({
 		where: {
 			user_id: req.session.user_id,
 		},
