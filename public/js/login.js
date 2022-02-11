3d374b05f7ddcceca9bb4e7c69776dd6722a969d
Reht Cavazos <reht.dev@gmail.com>
2022-02-11 13:46:11 -0600

    removing email fields. data sending and receiving on local host and insomnia

--- public/js/login.js
+++ public/js/login.js
@@ -1,7 +1,7 @@
 async function loginFormHandler(event) {
 	event.preventDefault();
 
-	const username = document.querySelector("#email-login").value.trim();
+	const username = document.querySelector("#user-login").value.trim();
 	const password = document.querySelector("#password-login").value.trim();
 
 	if (username && password) {
@@ -13,10 +13,12 @@ async function loginFormHandler(event) {
 			}),
 			headers: { "Content-Type": "application/json" },
 		});
-
+		
 		if (response.ok) {
+			console.log("logged in!");
 			document.location.replace("/dashboard/");
 		} else {
+			console.log("wrong password");
 			alert(response.statusText);
 		}
 	}
