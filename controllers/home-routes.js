7cc822acba247f4e636ce55bacdb790e194376d0
Reht Cavazos <reht.dev@gmail.com>
2022-02-10 20:11:12 -0600

    fixed signup errors with reid

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,21 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-  res.render('homepage');
+  res.render('homepage', {
+    created_at: new Date(),
+    user: {
+      username: 'test_user'
+    }
+  });
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
 });
 
 module.exports = router;
