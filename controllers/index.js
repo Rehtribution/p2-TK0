734f67c3bec2e1db1e3c02f1f529480cc7b679fb
pdguerra12 <paul.guerra09@gmail.com>
2022-02-11 11:43:32 -0600

    dashboard and home routes created, controllers/index updated

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,11 @@
-const router = require('express').Router();
-const apiRoutes = require('./api/');
-router.use('/api', apiRoutes);
+const router = require("express").Router();
+
+const apiRoutes = require("./api/");
+const homeRoutes = require("./home-routes.js");
+const dashboardRoutes = require("./dashboard-routes.js");
+
+router.use("/dashboard", dashboardRoutes);
+router.use("/api", apiRoutes);
+router.use("/", homeRoutes);
 
 module.exports = router;
