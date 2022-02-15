604c30ebdd5e9df392910707f28b464f4d92cb34
pdguerra12 <paul.guerra09@gmail.com>
2022-02-15 14:53:40 -0600

    login and signup revert

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,11 +1,11 @@
-async function loginFormHandler(event) {
+async function signupFormHandler(event) {
 	event.preventDefault();
 
-	const username = document.querySelector("#username-login").value.trim();
-	const password = document.querySelector("#password-login").value.trim();
+	const username = document.querySelector("#username-signup").value.trim();
+	const password = document.querySelector("#password-signup").value.trim();
 
 	if (username && password) {
-		const response = await fetch("/api/users/login", {
+		const response = await fetch("/api/users", {
 			method: "post",
 			body: JSON.stringify({
 				username,
@@ -23,5 +23,5 @@ async function loginFormHandler(event) {
 }
 
 document
-	.querySelector(".login-form")
-	.addEventListener("submit", loginFormHandler);
+	.querySelector(".signup-form")
+	.addEventListener("submit", signupFormHandler);
