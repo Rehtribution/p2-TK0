ed69e66df23d6bef8cadeec61fbc261b3df237cd
Reht Cavazos <reht.dev@gmail.com>
2022-02-15 20:21:55 -0600

    comments

--- controllers/api/joke-routes.js
+++ controllers/api/joke-routes.js
@@ -1,13 +1,15 @@
 const router = require("express").Router();
 
+// flow of data 3: brings in the npm package to access it
 var oneLinerJoke = require("one-liner-joke");
 
-
+// accesses the package data and returns a result
+// this result is requested from the script.js
 router.get("/", (req, res) => {
-    var getRandomJoke = oneLinerJoke.getRandomJoke({
-        exclude_tags: ["dirty", "racist", "sex"],
-    });
-    console.log(getRandomJoke.body);
+  var getRandomJoke = oneLinerJoke.getRandomJoke({
+    exclude_tags: ["dirty", "racist", "sex"],
+  });
+  // console.log(getRandomJoke.body);
   res.json(getRandomJoke.body);
 });
 
