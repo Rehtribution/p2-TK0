9e565f639017059c0f332de1cbca847e93500993
Reht Cavazos <reht.dev@gmail.com>
2022-02-17 12:47:36 -0600

    joke button connected and functioning properly

--- public/js/script.js
+++
@@ -1,8 +0,0 @@
-// connects to the dashboard.handlebars
-let jokeContainer = document.querySelector(".joke-container")
-console.log(jokeContainer.innerText);
-// flow of data 2 requests the code from the api/jokes -> go to the joke-route
-fetch('/api/jokes')
-    // returns the response from the api/jokes
-  .then(response => response.json())
-  .then(data => jokeContainer.innerText = decodeURI(data));
